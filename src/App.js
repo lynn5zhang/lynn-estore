f1ae860c263f3a424c918b53227a9e88eba8ea5a
lynn0zhang <linda55lynn@gmail.com>
2022-02-20 11:09:00 -0800

    add dependecies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
 import './App.css';
+import LandingPage from './components';
+import ProductDetails from './components/Product/productDetails';
+import Cart from './components/Cart';
+import PaymentResponse from "./components/Cart/paymentResponse";
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+
+      <Switch>
+        <Route path="/estore" component={LandingPage} />
+        {/* <Route exact path="/productdetails" component={ProductDetails} />
+          <Route exact path="/viewcart" component={Cart}/>
+          <Route exact path="/paymentresponse" component={PaymentResponse}/> */}
+        <Route exact path="/" component={Login} />
+      </Switch>
+
+    </>
   );
 }
 
